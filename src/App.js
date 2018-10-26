dc3ecba7db7b86410f9434ed311c1f54015bbd64
Francis Coteur <fcoteur@hotmail.com>
2018-10-26 07:30:21 +0200

    basic setup of the container app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//import ChildComponent from './ChildComponent'
+import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      power: true,
+      volume: 5,
+      display: '',
+      soundBank: [
+        {
+          key: 'Q',
+          urlSound: ''
+        }
+      ]
+    }
+  }
   render() {
     return (
-      <div id="drum-machine">
-        <Display />
-        <DrumPads />
-        <Commands />
+      <div id='display' className='flex-container'>
+          <h1>FCC Drum Machine!</h1>
+          <Display />
+          <Control />
+          <SoundPads />
       </div>
     );
   }
 }
 
 export default App;
+
